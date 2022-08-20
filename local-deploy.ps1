0d897cf74eaf10579c09adeb37dd66cf109ea070
khalid ait bellahs <kaitbellahs@gmail.com>
2022-08-20 02:47:40 -0300

    Update

--- local-deploy.ps1
+++ local-deploy.ps1
@@ -14,7 +14,6 @@ if($args.count -gt 0){
 docker build -t hub.docker.internal:5000/ikomida/rabbitmq-image:latest . $nocache
 ThrowOnNativeFailure
 docker push hub.docker.internal:5000/ikomida/rabbitmq-image:latest
-docker pull hub.docker.internal:5000/ikomida/rabbitmq-image:latest
 kubectl -n ikomida delete StatefulSet rabbitmq-microservice
 kubectl apply -f ./k8s-dev/deployment.yaml
 kubectl apply -f ./k8s-dev/service.yaml
